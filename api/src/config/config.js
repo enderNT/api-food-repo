09bba5ff478bb0ca93b1783b7f95337669c6c8c7
Gabriel <gayagocr@gmail.com>
2022-04-07 18:24:44 -0500

    correction

--- api/src/config/config.js
+++ api/src/config/config.js
@@ -49,16 +49,31 @@ module.exports = {
       freezeTableName: true,
     }
   },
-  "production": {
+  production: {
     username: `${DB_USERNAME}`,
     password: `${DB_PASSWORD}`,
     database: `${DB_NAME}`,
     host: `${DB_HOST}`,
     dialect: `${DB_DIALECT}`,
+    port: 5432,
     logging: false,
+    pool: {
+      max: 3,
+      min: 1,
+      idle: 10000,
+    },
     define: {
       timestamps: false,
       freezeTableName: true,
-    }
+    },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        // Ref.: https://github.com/brianc/node-postgres/issues/2009
+        rejectUnauthorized: false,
+      },
+      keepAlive: true,
+    },
+    ssl: true,
   },
 }
